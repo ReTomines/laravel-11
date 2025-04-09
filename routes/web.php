b83fbcb7b996571f866d501487d52b7404fd8098
ReTomines <renatatr.concurso@gmail.com>
2025-04-09 17:51:20 -0300

    commit em 09/04 - 17/51

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get ('rota/', ['nome do controller', 'nome do método']);
+Route::get('admin/usuarios', [UserController::class, 'index']);
+Route::get('admin/usuarios/{user}', [UserController::class, 'show']);
