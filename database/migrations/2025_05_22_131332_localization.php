6bd397026944366127621f7f47dfc808950e83c0
ReTomines <renatatr.concurso@gmail.com>
2025-05-22 10:20:31 -0300

    3º commit - proj. videoWall [tabela localização]

---
+++ database/migrations/2025_05_22_131332_localization.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('localization', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->timestamps();
+        });
+    
+        // Inserção direta dos dados fixos
+        DB::table('localization')->insert([
+            ['nome' => '1º PAVIMENTO'],
+            ['nome' => '2º PAVIMENTO'],
+            ['nome' => '3º PAVIMENTO'],
+            ['nome' => '4º PAVIMENTO'],
+            ['nome' => '5º PAVIMENTO'],
+            ['nome' => '6º PAVIMENTO'],
+            ['nome' => '7º PAVIMENTO'],
+            ['nome' => '8º PAVIMENTO'],
+            ['nome' => '9º PAVIMENTO'],
+            ['nome' => '10º PAVIMENTO'],
+            ['nome' => 'PALÁCIO'],
+            ['nome' => 'SUBSOLO'],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
