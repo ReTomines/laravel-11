44e2827ad40cf028ab56020f991d33bd869da9f9
ReTomines <renatatr.concurso@gmail.com>
2025-07-21 14:45:22 -0300

    13º commit - reesruturando ideia inicial

--- routes/web.php
+++ routes/web.php
@@ -45,8 +45,6 @@ Route::middleware(['auth'])->group(function () {
     Route::put('/videowall/vereadores/{vereador}', [VereadoresController::class, 'update'])->name('videowall.vereadores.update');
     // Fim Área do videowall ................................................................................................................
 
-    // Área Slides
-    Route::get('/slides', [SlidesController::class, 'index'])->name('slides.index');
 });
 
 
